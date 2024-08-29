3eee760c01e94b006a95b51db06b576052e739ef
KanyeFan30 <matthewkirkland049@gmai.com>
2024-08-29 23:32:10 +1000

    Updated to properly read scoreboard without emoji or colour formatters

--- src/main/java/com/github/kanyefan30/autoggmod/AutoGGMod.java
+++ src/main/java/com/github/kanyefan30/autoggmod/AutoGGMod.java
@@ -1,13 +1,10 @@
 package com.github.kanyefan30.autoggmod;
 
 import net.minecraft.client.Minecraft;
-import net.minecraft.scoreboard.Score;
 import net.minecraft.scoreboard.ScoreObjective;
 import net.minecraft.scoreboard.Scoreboard;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
-
-import java.util.Collection;
 
 public class AutoGGMod {
     private final Minecraft mc = Minecraft.getMinecraft();
@@ -19,18 +16,12 @@ public class AutoGGMod {
             Scoreboard scoreboard = mc.theWorld.getScoreboard();
             ScoreObjective objective = scoreboard.getObjectiveInDisplaySlot(1);
 
-            if (objective != null) {
-                Collection<Score> scores = scoreboard.getScores();
-                for (Score score : scores) {
-                    String lineText = score.getPlayerName();
-                    if (lineText.equals("Game ended!") && !gameEnded) {
-                        mc.thePlayer.sendChatMessage("/achat gg");
-                        gameEnded = true;
-                        break;
-                    } else if (!lineText.equals("Game ended!")) {
-                        gameEnded = false;
-                    }
-                }
+            String scoreboardText = ScoreboardReader.getScoreboard();
+            if (scoreboardText != null && scoreboardText.contains("Game ended!") && !gameEnded) {
+                mc.thePlayer.sendChatMessage("/achat gg");
+                gameEnded = true;
+            } else if (scoreboardText != null && !scoreboardText.contains("Game ended!")) {
+                gameEnded = false;
             }
         }
     }
